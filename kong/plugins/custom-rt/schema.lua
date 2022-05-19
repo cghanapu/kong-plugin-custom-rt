09bfb1e884651593d724f7e7b7fd5fdd602e1a00
cghanapu <caitanyakrishna_ghanapuram@optum.com>
2022-05-18 21:15:20 -0500

    rt

--- kong/plugins/custom-rt/schema.lua
+++ kong/plugins/custom-rt/schema.lua
@@ -22,10 +22,7 @@ return {
           { content_type = { type = "string" }, },
           { body = { type = "string" }, },
           { echo = { type = "boolean", required = true, default = false }, },
-          { trigger = typedefs.header_name },
-          { trigger_value = { type = "string" }, },
-          { avert = typedefs.header_name },
-          { avert_value = { type = "string" }, }
+          { trigger = typedefs.header_name }
         },
         custom_validator = function(config)
           if is_present(config.message)
